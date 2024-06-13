2f3096d3c6885e186bc4b71ef5d9e59846a4adf6
ASTUT0 <noahlezama2000@gmail.com>
2024-06-13 11:25:52 +0200

    Update Recipies.java

--- Java/RecipeApplication/src/Recipies/Recipies.java
+++ Java/RecipeApplication/src/Recipies/Recipies.java
@@ -4,49 +4,51 @@ package Recipies;
  *
  * @author Noah
  */
+import java.net.URL;
 import java.util.ArrayList;
 
 public class Recipies {
 
     private static int autoIncrementId = 0;
     private int id;
-    private String Name = "";
-    private String Description = "";
-    private ArrayList<String> Ingredients = new ArrayList<String>();
-    private int Duration;
-    private int Persons;
+    private String name = "";
+    private String description = "";
+    private ArrayList<String> ingredients = new ArrayList<String>();
+    private int duration;
+    private int persons;
+    private URL database =  / waiting / api / database.json;
 
-    public Recipies(String Name, String Description, int Duration, int Persons) {
+    public Recipies(String name, String description, int duration, int persons, String database) {
         this.id = ++autoIncrementId;
-        this.Name = Name;
-        this.Description = Description;
-        this.Duration = Duration;
-        this.Persons = Persons;
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.persons = persons;
     }
 
-    public void insert(String Name, String Description, int Duration) {
-        // Implementación
+    public void insert(String name, String description, int duration) {
+        // Implementation
     }
 
     public void delete(int id) {
-        // Implementación
+        // Implementation
     }
 
-    public void replace(String Name, String Description, int Duration) {
-        // Implementación
+    public void replace(String name, String description, int duration) {
+        // Implementation
     }
 
-    public void filter(ArrayList<String> Ingredients) {
-        // Implementación
+    public void filter(ArrayList<String> ingredients) {
+        // Implementation
     }
 
-    public void search(String Name) {
-        // Implementación
+    public void search(String name) {
+        // Implementation
     }
 
-    // On sortCriteria we cat chose between "ID" or "Name" & ascending(True) or descending(False).
+    // On sortCriteria we cat chose between "ID" or "name" & ascending(True) or descending(False).
     public void sort(String sortCriteria, boolean ascending) {
-        // Implementación
+        // Implementation
     }
 
 }
