55cc17dacb2fc9b8218c97c01b4c15513c0f80d6
ASTUT0 <noahlezama2000@gmail.com>
2024-06-17 15:53:13 +0200

    Updates

--- Java/RecipeApplication/src/Recipies/Main.java
+++ Java/RecipeApplication/src/Recipies/Main.java
@@ -4,6 +4,8 @@
  */
 package Recipies;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Noah
@@ -11,6 +13,10 @@ package Recipies;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<String> ingredients = new ArrayList<>();
+        ingredients.add("Flour");
+        ingredients.add("Sugar");
 
+        Recipies.insert("Cake", "A delicious cake", 60, 4, ingredients);
     }
 }
